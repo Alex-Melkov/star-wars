cfcc9ab921993ec2db24c7d84a0a535943d4e62b
Alex Melkov <alex.matvey93@gmail.com>
2023-04-03 19:48:21 +0700

    add page NotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Star from '../../images/death-star.png';
 
 import styles from './NotFound.module.scss';
+import BtnNav from '../../components/BtnNav/BtnNav';
 
 const NotFound: React.FC = () => {
   return (
-    <div className={styles.notFoundContainer}>NotFound</div>
+    <div className={styles.wrapper}> 
+      <div className={styles.notFoundContainer}>
+        <div className={styles.text}>404</div>
+        <img src={Star} className={styles.deathStar} alt="Death Star" />
+      </div>
+      <div className={styles.btnBlock}>
+        <BtnNav children="Return" backgroundColor="#73D677" path="/" />
+      </div>
+    </div>
   )
 }
 
